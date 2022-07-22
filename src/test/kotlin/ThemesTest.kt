6f48ce4ba20107d6938220925f379a40aa354cf6
ZacharyChin <vcoty233@163.com>
2022-07-22 11:04:34 +0800

    Init the test cases of Themes.

---
+++ src/test/kotlin/ThemesTest.kt
@@ -0,0 +1,122 @@
+import org.codroid.textmate.Theme
+import kotlin.test.Test
+
+class ThemesTest {
+    @Test
+    fun `Theme matching gives higher priority to deeper matches`() {
+
+    }
+
+    @Test
+    fun `Theme matching gives higher priority to parent matches 1`() {
+
+    }
+
+    @Test
+    fun `Theme matching gives higher priority to parent matches 2`() {
+
+    }
+
+    @Test
+    fun `Theme matching can match`() {
+
+    }
+
+    @Test
+    fun `Theme matching Microsoft vscode#23460`() {
+
+    }
+
+    @Test
+    fun `Theme parsing can parse`() {
+
+    }
+
+    @Test
+    fun `Theme resolving strcmp works`() {
+
+    }
+
+    @Test
+    fun `Theme resolving strArrCmp works`() {
+
+    }
+
+    fun assertThemeEqual(actual: Theme, expected: Theme) {
+
+    }
+
+    @Test
+    fun `Theme resolving always has defaults`() {
+
+    }
+
+    @Test
+    fun `Theme resolving respects incoming defaults 1`() {
+
+    }
+
+    @Test
+    fun `Theme resolving respects incoming defaults 2`() {
+
+    }
+
+    @Test
+    fun `Theme resolving respects incoming defaults 3`() {
+
+    }
+
+    @Test
+    fun `Theme resolving respects incoming defaults 4`() {
+
+    }
+
+    @Test
+    fun `Theme resolving respects incoming defaults 5`() {
+
+    }
+
+    @Test
+    fun `Theme resolving can merge incoming defaults`() {
+
+    }
+
+    @Test
+    fun `Theme resolving defaults are inherited`() {
+
+    }
+
+    @Test
+    fun `Theme resolving same rules get merged`() {
+
+    }
+
+    @Test
+    fun `Theme resolving rules are inherited 1`() {
+
+    }
+
+    @Test
+    fun `Theme resolving rules are inherited 2`() {
+
+    }
+
+    @Test
+    fun `Theme resolving rules with parent scopes`() {
+
+    }
+
+    @Test
+    fun `Theme resolving issue #38, ignores rules with invalid colors`() {
+
+    }
+
+    @Test
+    fun `Theme resolving issue #35, Trailing comma in a tmTheme scope selector`() {
+
+    }
+
+
+
+
+}
