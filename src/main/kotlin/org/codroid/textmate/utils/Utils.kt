d045dd17fb5e36b61da07900b038230f45e8ade5
ZacharyChin <vcoty233@163.com>
2022-07-22 15:45:59 +0800

    Completed Utils.

--- src/main/kotlin/org/codroid/textmate/utils/Utils.kt
+++ src/main/kotlin/org/codroid/textmate/utils/Utils.kt
@@ -1,19 +1,28 @@
 package org.codroid.textmate.utils
 
-fun <T> clone(something: T): T {
-    TODO()
+inline fun <reified T : Any> T.clone(): T {
+    return doClone(this) as T
 }
 
-private fun Any.doClone(): Any {
-    TODO()
+fun doClone(something: Any): Any {
+    return if (something is Array<*>) {
+        something.cloneArray()
+    } else {
+        something.clone()
+    }
 }
 
-private fun Array<Any>.cloneArray(): Any {
-    TODO()
-}
-
-private fun Any.cloneObj(): Any {
-    TODO()
+private fun Array<*>.cloneArray(): Array<Any> {
+    val r = arrayOf<Any>(this.size)
+    var i = 0
+    val len: Int = this.size
+    while (i < len) {
+        this[i]?.let {
+            r[i] = doClone(it)
+        }
+        i++
+    }
+    return r
 }
 
 fun Any.mergeObjects(vararg sources: Array<Any>): Any {
@@ -21,12 +30,47 @@ fun Any.mergeObjects(vararg sources: Array<Any>): Any {
 }
 
 fun basename(path: String): String {
-    TODO()
+    return when (val idx = path.lastIndexOf('/').inv() or path.lastIndexOf('\\').inv()) {
+        0 -> path
+        (path.length - 1).inv() -> basename(path.substring(0, path.length - 1))
+        else -> path.substring(idx.inv() + 1)
+    }
 }
 
-private var CAPTURING_REGEX_SOURCE = "\\\$(\\d+)|\\\${(\\d+):\\/(downcase|upcase)}";
+private var CAPTURING_REGEX_SOURCE = Regex("\\\$(\\d+)|\\\$*(\\d+):/(downcase|upcase)}")
 
-class RegexSource {}
+object RegexSource {
+    fun hasCaptures(regexSource: String?): Boolean {
+        if (regexSource == null) return false
+        return CAPTURING_REGEX_SOURCE.matches(regexSource)
+    }
+
+    fun replaceCaptures(
+        regexSource: String, captureSource: String, captureIndices: Array<OnigCaptureIndex>
+    ): String {
+        return regexSource.replace(CAPTURING_REGEX_SOURCE) {
+            val capture = captureIndices.getOrNull(
+                Integer.parseInt(
+                    it.groupValues.getOrNull(0) ?: it.groupValues.getOrElse(1) { "0" }, 10
+                )
+            )
+            return@replace if (capture != null) {
+                var result = captureSource.substring(capture.start, capture.end)
+                // Remove leading dots that would make the selector invalid
+                while (result[0] == '.') {
+                    result = result.substring(1)
+                }
+                when (it.groupValues.getOrElse(2) { false }) {
+                    "downcase" -> result.lowercase()
+                    "upcase" -> result.uppercase()
+                    else -> result
+                }
+            } else {
+                it.value
+            }
+        }
+    }
+}
 
 /**
  * A union of given const enum values.
@@ -34,25 +78,61 @@ class RegexSource {}
 typealias OrMask = Int
 
 fun strcmp(a: String, b: String): Int {
-    TODO()
+    return if (a < b) -1
+    else if (a > b) 1
+    else 0
 }
 
 fun strArrCmp(a: Array<String>?, b: Array<String>?): Int {
-    TODO()
+    if (a == null && b == null) return 0
+    else if (a == null) return -1
+    else if (b == null) return 1
+
+    val len1 = a.size
+    val len2 = b.size
+    if (len1 == len2) {
+        for (i in 0 until len1) {
+            val res = strcmp(a[i], b[i])
+            if (res != 0) {
+                return res
+            }
+        }
+        return 0
+    }
+    return len1 - len2
 }
 
 fun isValidHexColor(hex: String): Boolean {
-    TODO()
+    return if (Regex("^#[\\da-f]{6}\$", RegexOption.IGNORE_CASE).matches(hex)) {
+        true
+    } else if (Regex("^#[\\da-f]{8}\$", RegexOption.IGNORE_CASE).matches(hex)) {
+        true
+    } else if (Regex("^#[\\da-f]{3}\$", RegexOption.IGNORE_CASE).matches(hex)) {
+        true
+    } else {
+        Regex("^#[\\da-f]{4}\$", RegexOption.IGNORE_CASE).matches(hex)
+    }
+
 }
 
 /**
  * Escapes regular expression characters in a given string
  */
-fun escapeRegExpCharacters(value: String): String {
-    TODO()
-}
+fun escapeRegExpCharacters(value: String): String =
+    value.replace(Regex("[\\-\\\\{}*+?|^$.,\\[\\]()#\\s]"), "\\$&")
 
-class CachedFn<K, V> {}
+class CachedFn<K, V>(private val fn: (key: K) -> V) {
+    private val cache = HashMap<K, V>()
+
+    fun get(key: K): V {
+        if (cache.containsKey(key)) {
+            return cache[key]!!
+        }
+        val value = fn(key)
+        cache[key] = value
+        return value
+    }
+}
 
 private var performance: (() -> Int)? = null
 
